81ffac2952f24b3dbbeb063c5a11e406781218cd
Benkolon152 <v.benasz@gmail.com>
2025-09-19 10:52:43 +0200

    implement GET /teachers

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+var global= {
+    teachers: [
+        {
+            name: "John Doe",
+            hisClass: "13/A",
+            favorite: false,
+        },
+        {
+            name: "Jane Doe",
+            hisClass: "18/C",
+            favorite: true,
+        }, 
+    ],
+    //favoriteTeacherIdx: null,
+}
+
 app.get('/teachers', (req, res)=>{
-    res.sendStatus(200)
+    res.status(200).json(global.teachers);
 })
 
 const port = 3333;
