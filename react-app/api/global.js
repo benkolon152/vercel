86b1f95cd569f0ace50b61276975aa14b858e578
Benkolon152 <v.benasz@gmail.com>
2025-09-19 12:26:18 +0200

    Global teachers

---
+++ react-app/api/global.js
@@ -0,0 +1,15 @@
+var globalTeachers= {
+    teachers: [
+        {
+            name: "John Doe",
+            hisClass: "13/A",
+            favorite: false,
+        },
+        {
+            name: "Jane Doe",
+            hisClass: "18/C",
+            favorite: true,
+        }, 
+    ],
+    //favoriteTeacherIdx: null,
+}
